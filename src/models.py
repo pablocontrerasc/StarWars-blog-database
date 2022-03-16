286e6ae494c416766ff22706094b0e8d84e17b09
pablocontrerasc <cont.pab@gmail.com>
2022-03-16 15:18:31 +0000

    diagrama ok

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,62 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+association_table = Table('personajes_favorito', Base.metadata,
+                          Column('usuario_id', ForeignKey('usuario.id')),
+                          Column('personaje_id', ForeignKey('personaje.id'))
+                          )
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(100))
+    apellido = Column(String(100))
+    email = Column(String(150))
+    password = Column(String(150))
+    fecha_subscripcion = Column(String(100))
+    children = relationship('Personaje',
+                            secondary=association_table)
+    children = relationship('Planeta',
+                            secondary=association_table)
+
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    height = Column(String(100))
+    mass = Column(String(100))
+    hair_color = Column(String(100))
+    skin_color = Column(String(100))
+    eye_color = Column(String(100))
+    birth_year = Column(String(100))
+    gender = Column(String(100))
+    homeworld = Column(String(150))
+    created = Column(String(100))
+
+
+association_table = Table('planeta_favorito', Base.metadata,
+                          Column('usuario_id', ForeignKey('usuario.id')),
+                          Column('planeta_id', ForeignKey('planeta.id'))
+                          )
+
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    orbital_period = Column(String(100))
+    rotation_period = Column(String(100))
+    diameter = Column(String(100))
+    climate = Column(String(100))
+    gravity = Column(String(100))
+    terrain = Column(String(100))
+    surface_water = Column(String(100))
+    population = Column(String(100))
+    created = Column(String(100))
 
     def to_dict(self):
         return {}
-
-## Draw from SQLAlchemy base
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
